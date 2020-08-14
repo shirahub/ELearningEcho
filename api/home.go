ff74ba14995562a0b9efc8466a84016fb418aa47
myUser <shirleen.adriana@gmail.com>
2020-08-14 11:04:00 +0700

    login prototype done

--- api/home.go
+++ api/home.go
@@ -11,8 +11,3 @@ func HomePage(c echo.Context) error {
 	data := model.M{"message": "func HomePage"}
 	return c.Render(http.StatusOK, "login.html", data)
 }
-
-// e.GET("/index", func(c echo.Context) error {
-// 	data := M{"message": "Hello World!"}
-// 	return c.Render(http.StatusOK, "index.html", data)
-// })
--- api/login.go
+++ api/login.go
@@ -17,11 +17,7 @@ func UserLogin(c echo.Context) error {
 		return err
 	}
 
-	fmt.Println("Go MySQL Tutorial")
-
 	// Open up our database connection.
-	// I've set up a database on my local machine using phpmyadmin.
-	// The database is called testDb
 	db, err := sql.Open("mysql", "root:120625@/elearning")
 
 	// if there is an error opening the connection, handle it
@@ -30,13 +26,14 @@ func UserLogin(c echo.Context) error {
 	}
 
 	// defer the close till after the main function has finished
-	// executing
 	defer db.Close()
 
+	//single row query lalu langsung parse ke model.UserData
+	// referensi: http://go-database-sql.org/retrieving.html
 	var userData model.UserData
 	getUserData := db.QueryRow("SELECT username, fullname FROM user WHERE username=?", user.Username).Scan(&userData.Username, &userData.Fullname)
 	if getUserData != nil {
-		//fmt.Println("ngga ketemu")
+		// kalau ga ketemu / nil, kirim message NOT FOUND
 		log.Fatal(err)
 		data := model.M{"message": "NOT FOUND"}
 
@@ -44,6 +41,7 @@ func UserLogin(c echo.Context) error {
 	}
 	fmt.Println(userData)
 
+	// kalau ketemu, kirim userData yang didapatkan
 	data := model.M{"message": userData}
 	return c.Render(http.StatusOK, "user.html", data)
 }
